74e82d81c7ac9e09b688544441c61591bc26f0af
Alex | Kronox <alexander_vogt@outlook.de>
2024-11-27 19:34:11 +0100

    2021 23,24

--- 2021/24/task.ts
+++ 2021/24/task.ts
@@ -1,129 +1,57 @@
 export async function taskOne(input: string[]): Promise<void> {    
-    const ins = input.map(i => i.split(' '))
-
-    const badStates = new Set<string>()
-    console.log(run(0, {'w':0,'x':0,'y':0,'z':0}))
-
-    function run(i: number, register: Record<string, number>): string|false {
-        const k = i + "|" + register['z'] + "|" + register['w']
-        if (badStates.has(k)) {
-            return false
-        }
-
-        while(i < ins.length) {
-            const task = ins[i][0]
-            if (task == 'inp') {
-                for (const n of [9,8,7,6,5,4,3,2,1]) {
-                    register[ins[i][1]] = n
-                    const r = run(i+1, copyReg(register))
-                    if (r === false) continue
-                    else {
-                        return `${n}${r}`
-                    }
-                }
-                badStates.add(k)
-                return false
-            } else {
-                const a = ins[i][1]
-                const b = ins[i][2]
-                if (task == 'add') {
-                    register[a] += get(b)
-                } else if (task == 'mul') {
-                    register[a] *= get(b)
-                } else if (task == 'mod') {
-                    register[a] %= get(b)
-                } else if (task == 'div') {
-                    register[a] = Math.floor(get(a) / get(b))
-                } else if (task == 'eql') {
-                    register[a] = get(a) == get(b) ? 1:0
-                } else {
-                    throw i
-                }
-                i++
-            }
-        }
-        if (register['z'] != 0) {
-            return false
-        }
-        return ''
-        
-        function get(x: string) {
-            if (x.match(/[wxyz]/)) {
-                return register[x]
-            } else {
-                return Number(x)
-            }
-        }
-    }
-
-    function copyReg(reg: Record<string, number>) {
-        return {
-            'w': reg['w'],'x': reg['x'],'y': reg['y'],'z': reg['z']
-        }
-    } 
+    task(input, [9,8,7,6,5,4,3,2,1])
+}
+export async function taskTwo(input: string[]): Promise<void> {
+    task(input, [1,2,3,4,5,6,7,8,9])
 }
 
-export async function taskTwo(input: string[]): Promise<void> {
-    const ins = input.map(i => i.split(' '))
+export async function task(input: string[], order: number[]) {
+    const xAdds = [] as number[]
+    const yAdds = [] as number[]
+    const zDivs = [] as number[]
+
+    for (let d = 0; d < 14; d++) {
+        zDivs.push(Number(input[d*18+4].split(' ')[2]))
+        xAdds.push(Number(input[d*18+5].split(' ')[2]))
+        yAdds.push(Number(input[d*18+15].split(' ')[2]))
+    }
+    console.log(xAdds, yAdds, zDivs)
 
     const badStates = new Set<string>()
-    console.log(run(0, {'w':0,'x':0,'y':0,'z':0}))
+    console.log(run(0, 0, 0))
 
-    function run(i: number, register: Record<string, number>): string|false {
-        const k = i + "|" + register['z'] + "|" + register['w']
-        if (badStates.has(k)) {
-            return false
-        }
+    function run(depth: number, curTotal: number, lastZ: number): number|false {
+        if (depth >= 14) return lastZ == 0 ? curTotal:false
+        const k = key(depth, lastZ)
+        if(badStates.has(k)) return false
+        const t = curTotal * 10
 
-        while(i < ins.length) {
-            const task = ins[i][0]
-            if (task == 'inp') {
-                for (const n of [1,2,3,4,5,6,7,8,9]) {
-                    register[ins[i][1]] = n
-                    const r = run(i+1, copyReg(register))
-                    if (r === false) continue
-                    else {
-                        return `${n}${r}`
-                    }
-                }
-                badStates.add(k)
-                return false
-            } else {
-                const a = ins[i][1]
-                const b = ins[i][2]
-                if (task == 'add') {
-                    register[a] += get(b)
-                } else if (task == 'mul') {
-                    register[a] *= get(b)
-                } else if (task == 'mod') {
-                    register[a] %= get(b)
-                } else if (task == 'div') {
-                    register[a] = Math.floor(get(a) / get(b))
-                } else if (task == 'eql') {
-                    register[a] = get(a) == get(b) ? 1:0
-                } else {
-                    throw i
-                }
-                i++
+        for (const i of order) {
+            let w = i
+            let z = lastZ
+            let x = z
+            x %= 26
+            z = Math.floor(z / zDivs[depth])
+            x += xAdds[depth]
+            x = x == w ? 0:1
+            let y = 25
+            y *= x
+            y += 1
+            z *= y
+            y = w + yAdds[depth]
+            y *= x
+            z += y
+
+            const r = run(depth+1, t+i, z)
+            if(r !== false) {
+                return r
             }
         }
-        if (register['z'] != 0) {
-            return false
-        }
-        return ''
-        
-        function get(x: string) {
-            if (x.match(/[wxyz]/)) {
-                return register[x]
-            } else {
-                return Number(x)
-            }
-        }
+        badStates.add(k)
+        return false
     }
 
-    function copyReg(reg: Record<string, number>) {
-        return {
-            'w': reg['w'],'x': reg['x'],'y': reg['y'],'z': reg['z']
-        }
-    } 
+    function key(...n: number[]) {
+        return n.join('|')
+    }
 }
