4f072401bd3ab9254a3a8ee78d5366c41849e663
Alex | Kronox <alexander_vogt@outlook.de>
2024-12-07 05:50:41 +0100

    fix base

--- base/index.ts
+++ base/index.ts
@@ -1,3 +1,2 @@
 export * from './math'
-export * from './parse'
 export * from './simpleStructure'
--- base/parse.ts
+++
@@ -1,5 +0,0 @@
-function parseNumberList(input: string, seperator: string|RegExp = /,| /g) {
-  return input.split(seperator).filter(s => s != '').map(i => parseInt(i.trim()))
-}
-
-export { parseNumberList }
